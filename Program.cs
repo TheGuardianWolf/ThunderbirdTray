013647dd5cedf3432ab3f4ed07e4c538fb17f317
TheGuardianWolf <lichking21st@windowslive.com>
2019-11-22 01:31:35 +1300

    Prevent multiple instances from running

--- Program.cs
+++ Program.cs
@@ -1,23 +1,32 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace ThunderbirdTray
 {
     static class Program
     {
+        static string appGuid = "299462ce-7579-45d0-afcc-8ea50db9a11b";
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new TrayBird());
+            using (Mutex mutex = new Mutex(false, "Global\\" + appGuid))
+            {
+                if (!mutex.WaitOne(0, false))
+                {
+                    MessageBox.Show("Instance already running");
+                    return;
+                }
+
+                Application.SetHighDpiMode(HighDpiMode.SystemAware);
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new TrayBird());
+            }
         }
     }
 }
