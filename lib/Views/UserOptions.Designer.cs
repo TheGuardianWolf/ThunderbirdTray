7ed1a69626311ce24c35b659887c69f748f9f90a
Will Bennion <will.bennion@outlook.com>
2020-10-01 12:15:31 +0100

    Add setting for specifying Thunderbird path.

--- lib/Views/UserOptions.Designer.cs
+++ lib/Views/UserOptions.Designer.cs
@@ -37,17 +37,24 @@
             this.trayBirdBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.HookMethodComboBox = new System.Windows.Forms.ComboBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.SelectPathButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ThunderbirdPathTextbox = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trayBirdBindingSource)).BeginInit();
             this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.checkboxMinimiseThundebird);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(9, 10);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(258, 70);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(194, 57);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Startup";
@@ -58,18 +65,20 @@
             this.checkboxMinimiseThundebird.Checked = global::ThunderbirdTray.Properties.Settings.Default.MinimiseOnStart;
             this.checkboxMinimiseThundebird.CheckState = System.Windows.Forms.CheckState.Checked;
             this.checkboxMinimiseThundebird.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::ThunderbirdTray.Properties.Settings.Default, "MinimiseOnStart", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-            this.checkboxMinimiseThundebird.Location = new System.Drawing.Point(17, 34);
+            this.checkboxMinimiseThundebird.Location = new System.Drawing.Point(13, 28);
+            this.checkboxMinimiseThundebird.Margin = new System.Windows.Forms.Padding(2);
             this.checkboxMinimiseThundebird.Name = "checkboxMinimiseThundebird";
-            this.checkboxMinimiseThundebird.Size = new System.Drawing.Size(232, 21);
+            this.checkboxMinimiseThundebird.Size = new System.Drawing.Size(176, 17);
             this.checkboxMinimiseThundebird.TabIndex = 0;
             this.checkboxMinimiseThundebird.Text = "Minimise Thunderbird on launch";
             this.checkboxMinimiseThundebird.UseVisualStyleBackColor = true;
             // 
             // SaveButton
             // 
-            this.SaveButton.Location = new System.Drawing.Point(84, 311);
+            this.SaveButton.Location = new System.Drawing.Point(63, 253);
+            this.SaveButton.Margin = new System.Windows.Forms.Padding(2);
             this.SaveButton.Name = "SaveButton";
-            this.SaveButton.Size = new System.Drawing.Size(90, 30);
+            this.SaveButton.Size = new System.Drawing.Size(68, 24);
             this.SaveButton.TabIndex = 1;
             this.SaveButton.Text = "OK";
             this.SaveButton.UseVisualStyleBackColor = true;
@@ -78,9 +87,10 @@
             // DiscardButton
             // 
             this.DiscardButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.DiscardButton.Location = new System.Drawing.Point(180, 311);
+            this.DiscardButton.Location = new System.Drawing.Point(135, 253);
+            this.DiscardButton.Margin = new System.Windows.Forms.Padding(2);
             this.DiscardButton.Name = "DiscardButton";
-            this.DiscardButton.Size = new System.Drawing.Size(90, 30);
+            this.DiscardButton.Size = new System.Drawing.Size(68, 24);
             this.DiscardButton.TabIndex = 2;
             this.DiscardButton.Text = "Cancel";
             this.DiscardButton.UseVisualStyleBackColor = true;
@@ -89,9 +99,11 @@
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.HookMethodComboBox);
-            this.groupBox2.Location = new System.Drawing.Point(12, 88);
+            this.groupBox2.Location = new System.Drawing.Point(9, 72);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(258, 73);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(194, 59);
             this.groupBox2.TabIndex = 3;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Window Detection Method";
@@ -102,23 +114,67 @@
             this.HookMethodComboBox.Items.AddRange(new object[] {
             "UI Automation",
             "Polling"});
-            this.HookMethodComboBox.Location = new System.Drawing.Point(17, 31);
+            this.HookMethodComboBox.Location = new System.Drawing.Point(13, 25);
+            this.HookMethodComboBox.Margin = new System.Windows.Forms.Padding(2);
             this.HookMethodComboBox.Name = "HookMethodComboBox";
-            this.HookMethodComboBox.Size = new System.Drawing.Size(232, 24);
+            this.HookMethodComboBox.Size = new System.Drawing.Size(175, 21);
             this.HookMethodComboBox.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.SelectPathButton);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.ThunderbirdPathTextbox);
+            this.groupBox3.Location = new System.Drawing.Point(9, 135);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox3.Size = new System.Drawing.Size(194, 93);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Thunderbird Path Override";
+            // 
+            // SelectPathButton
+            // 
+            this.SelectPathButton.Location = new System.Drawing.Point(113, 64);
+            this.SelectPathButton.Name = "SelectPathButton";
+            this.SelectPathButton.Size = new System.Drawing.Size(75, 23);
+            this.SelectPathButton.TabIndex = 7;
+            this.SelectPathButton.Text = "Select";
+            this.SelectPathButton.UseVisualStyleBackColor = true;
+            this.SelectPathButton.Click += new System.EventHandler(this.SelectPathButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Location = new System.Drawing.Point(10, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Supports environment variables";
+            // 
+            // ThunderbirdPathTextbox
+            // 
+            this.ThunderbirdPathTextbox.Location = new System.Drawing.Point(13, 38);
+            this.ThunderbirdPathTextbox.Name = "ThunderbirdPathTextbox";
+            this.ThunderbirdPathTextbox.Size = new System.Drawing.Size(175, 20);
+            this.ThunderbirdPathTextbox.TabIndex = 5;
             // 
             // UserOptions
             // 
             this.AcceptButton = this.SaveButton;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 353);
+            this.ClientSize = new System.Drawing.Size(212, 287);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.DiscardButton);
             this.Controls.Add(this.SaveButton);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "UserOptions";
@@ -129,6 +185,8 @@
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trayBirdBindingSource)).EndInit();
             this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -142,5 +200,9 @@
         private System.Windows.Forms.BindingSource trayBirdBindingSource;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.ComboBox HookMethodComboBox;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button SelectPathButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ThunderbirdPathTextbox;
     }
 }
--- lib/Views/UserOptions.cs
+++ lib/Views/UserOptions.cs
@@ -16,18 +16,38 @@ namespace ThunderbirdTray.Views
         {
             InitializeComponent();
             HookMethodComboBox.SelectedIndex = Properties.Settings.Default.HookMethod;
+            ThunderbirdPathTextbox.Text = Properties.Settings.Default.ThunderbirdPath;
         }
 
         private void SaveButton_Click(object sender, EventArgs e)
         {
+            var hasPathChanged = Properties.Settings.Default.ThunderbirdPath != ThunderbirdPathTextbox.Text;
+
             Properties.Settings.Default.HookMethod = HookMethodComboBox.SelectedIndex;
+            Properties.Settings.Default.ThunderbirdPath = ThunderbirdPathTextbox.Text;
             Properties.Settings.Default.Save();
+
+            if (hasPathChanged)
+                MessageBox.Show("Thunderbird path has been changed. ThunderbirdTray may require a restart.");
+
             Close();
         }
 
         private void CancelButton_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void SelectPathButton_Click(object sender, EventArgs e)
+        {
+            var picker = new OpenFileDialog
+            {
+                CheckFileExists = true,
+                Filter = "Thunderbird (thunderbird.exe)|thunderbird.exe"
+            };
+
+            if (picker.ShowDialog() == DialogResult.OK)
+                ThunderbirdPathTextbox.Text = picker.FileName;
         }
     }
 }
